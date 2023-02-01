7a7698daf0f699b3c58a9c76699dbeeb0421cc4d
Tunahan <ilyastunahanozcan@gmail.com>
2023-02-01 16:24:42 +0300

    Add project files.

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            DistributedCacheEntryOptions cacheOptions = new();
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            Product prod = new() { Price = 12, Name = "Urun1" ,Id=1};
+            string jsonProduct = JsonSerializer.Serialize(prod);
+
+            await _distributedCache.SetStringAsync("product:1", jsonProduct,cacheOptions);
+
+            //_distributedCache.SetString("name2", "Tunahan", cacheOptions);
+            //await _distributedCache.SetStringAsync("surname", "Ozcan", cacheOptions);
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("surname");
+
+            var jsonProd = _distributedCache.GetString("product:1");
+            var prod = JsonSerializer.Deserialize<Product>(jsonProd);
+            ViewBag.name = prod.Name;
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name2");
+            return View();
+        }
+        public IActionResult ImageUrl()
+        {
+            byte[] resimByte = _distributedCache.Get("resim");
+
+            return File(resimByte,"image/png");
+        }
+        public IActionResult ImageCache()
+        {
+            //görsel byte a dönüşecek
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/error2.png");
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("resim", imageByte);
+
+            return View();
+        }
+    }
+}
---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            if(!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+                MemoryCacheEntryOptions options=new();
+                options.AbsoluteExpiration=DateTime.Now.AddMinutes(1);
+                options.SlidingExpiration=TimeSpan.FromMinutes(10);
+                options.Priority=CacheItemPriority.High;
+
+
+
+                options.RegisterPostEvictionCallback((key,value,reason,state)=>{
+                    _memoryCache.Set("callback",$"{key}->{value} = sebep: {reason},state: {state}, options: {options}");
+
+                });
+                _memoryCache.Set("zaman", DateTime.Now.ToString(),options);
+            }
+
+
+            Product prod=new(){
+                Id=1,
+                Name="Laptop",
+                Price=5000
+            };
+            _memoryCache.Set<Product>("product:1",prod);
+
+
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue("zaman", out string zamanCache);
+            _memoryCache.TryGetValue("callback", out string callbackCache);
+            ViewBag.zaman= zamanCache;
+            ViewBag.callback= callbackCache;
+
+            ViewBag.product=_memoryCache.Get<Product>("product:1");
+            return View();
+        }
+    }
+}
